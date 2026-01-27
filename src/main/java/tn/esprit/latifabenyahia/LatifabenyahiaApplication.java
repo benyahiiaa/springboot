cb5f0121e62674835acff76fec27dbf98f4b088b
benyahiia <benyahia.latifa@esprit.tn>
2026-01-27 11:56:43 +0100

    Projet Spring Boot

---
+++ src/main/java/tn/esprit/latifabenyahia/LatifabenyahiaApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.latifabenyahia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LatifabenyahiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LatifabenyahiaApplication.class, args);
+    }
+
+}
